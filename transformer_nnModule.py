3c9e5f267ad7c857c3067baf0649396740d9b268
haizadtarik <haizadtarik@gmail.com>
2023-01-23 14:52:04 +0800

    Implement transformer model using pytorch module

---
+++ transformer_nnModule.py
@@ -0,0 +1,316 @@
+"""
+    Pytorch implementation from "A detailed guide to Pytorch's nn.Transformer() module" 
+    by Daniel Melchor: https://medium.com/p/c80afbc9ffb1/
+"""
+
+import torch
+from torch import nn, Tensor
+import torch.optim as optim
+import time
+
+import math
+import numpy as np
+import random
+
+# ========= Transformer model definition =========
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
+    
+class Transformer(nn.Module):
+    # Constructor
+    def __init__(self,num_tokens,d_model,num_heads,num_encoder_layers,num_decoder_layers,dropout,
+    ):
+        super().__init__()
+
+        # INFO
+        self.model_type = "Transformer"
+        self.dim_model = d_model
+
+        # LAYERS
+        self.positional_encoder = PositionalEncoding(
+            d_model=d_model, dropout=dropout, max_len=5000
+        )
+        self.embedding = nn.Embedding(num_tokens, d_model)
+        self.transformer = nn.Transformer(
+            d_model=d_model,
+            nhead=num_heads,
+            num_encoder_layers=num_encoder_layers,
+            num_decoder_layers=num_decoder_layers,
+            dropout=dropout,
+        )
+        self.out = nn.Linear(d_model, num_tokens)
+
+    def forward(self, src, tgt, tgt_mask=None, src_pad_mask=None, tgt_pad_mask=None):
+        # Src size must be (batch_size, src sequence length)
+        # Tgt size must be (batch_size, tgt sequence length)
+
+        # Embedding + positional encoding - Out size = (batch_size, sequence length, dim_model)
+        src = self.embedding(src) * math.sqrt(self.dim_model)
+        tgt = self.embedding(tgt) * math.sqrt(self.dim_model)
+        src = self.positional_encoder(src)
+        tgt = self.positional_encoder(tgt)
+        
+        # We could use the parameter batch_first=True, but our KDL version doesn't support it yet, so we permute
+        # to obtain size (sequence length, batch_size, dim_model),
+        src = src.permute(1,0,2)
+        tgt = tgt.permute(1,0,2)
+
+        # Transformer blocks - Out size = (sequence length, batch_size, num_tokens)
+        transformer_out = self.transformer(src, tgt, tgt_mask=tgt_mask, src_key_padding_mask=src_pad_mask, tgt_key_padding_mask=tgt_pad_mask)
+        out = self.out(transformer_out)
+        
+        return out
+      
+    def get_tgt_mask(self, size) -> torch.tensor:
+        # Generates a squeare matrix where the each row allows one word more to be seen
+        mask = torch.tril(torch.ones(size, size) == 1) # Lower triangular matrix
+        mask = mask.float()
+        mask = mask.masked_fill(mask == 0, float('-inf')) # Convert zeros to -inf
+        mask = mask.masked_fill(mask == 1, float(0.0)) # Convert ones to 0
+        
+        # EX for size=5:
+        # [[0., -inf, -inf, -inf, -inf],
+        #  [0.,   0., -inf, -inf, -inf],
+        #  [0.,   0.,   0., -inf, -inf],
+        #  [0.,   0.,   0.,   0., -inf],
+        #  [0.,   0.,   0.,   0.,   0.]]
+    
+        return mask
+    
+    def create_pad_mask(self, matrix: torch.tensor, pad_token: int) -> torch.tensor:
+        # If matrix = [1,2,3,0,0,0] where pad_token=0, the result mask is
+        # [False, False, False, True, True, True]
+        return (matrix == pad_token)
+
+# ======= Generate dummy sequence data =========  
+def generate_random_data(n):
+    SOS_token = np.array([2])
+    EOS_token = np.array([3])
+    length = 8
+
+    data = []
+
+    # 1,1,1,1,1,1 -> 1,1,1,1,1
+    for i in range(n // 3):
+        X = np.concatenate((SOS_token, np.ones(length), EOS_token))
+        y = np.concatenate((SOS_token, np.ones(length), EOS_token))
+        data.append([X, y])
+
+    # 0,0,0,0 -> 0,0,0,0
+    for i in range(n // 3):
+        X = np.concatenate((SOS_token, np.zeros(length), EOS_token))
+        y = np.concatenate((SOS_token, np.zeros(length), EOS_token))
+        data.append([X, y])
+
+    # 1,0,1,0 -> 1,0,1,0,1
+    for i in range(n // 3):
+        X = np.zeros(length)
+        start = random.randint(0, 1)
+
+        X[start::2] = 1
+
+        y = np.zeros(length)
+        if X[-1] == 0:
+            y[::2] = 1
+        else:
+            y[1::2] = 1
+
+        X = np.concatenate((SOS_token, X, EOS_token))
+        y = np.concatenate((SOS_token, y, EOS_token))
+
+        data.append([X, y])
+
+    np.random.shuffle(data)
+
+    return data
+
+
+def batchify_data(data, batch_size=16, padding=False, padding_token=-1):
+    batches = []
+    for idx in range(0, len(data), batch_size):
+        # We make sure we dont get the last bit if its not batch_size size
+        if idx + batch_size < len(data):
+            # Here you would need to get the max length of the batch,
+            # and normalize the length with the PAD token.
+            if padding:
+                max_batch_length = 0
+
+                # Get longest sentence in batch
+                for seq in data[idx : idx + batch_size]:
+                    if len(seq) > max_batch_length:
+                        max_batch_length = len(seq)
+
+                # Append X padding tokens until it reaches the max length
+                for seq_idx in range(batch_size):
+                    remaining_length = max_batch_length - len(data[idx + seq_idx])
+                    data[idx + seq_idx] += [padding_token] * remaining_length
+
+            batches.append(np.array(data[idx : idx + batch_size]).astype(np.int64))
+
+    print(f"{len(batches)} batches of size {batch_size}")
+
+    return batches
+
+
+train_data = generate_random_data(9000)
+val_data = generate_random_data(3000)
+
+print("train")
+print(train_data[:5])
+
+train_dataloader = batchify_data(train_data)
+val_dataloader = batchify_data(val_data)
+
+# ======= Train transformer model =========
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = Transformer(
+    num_tokens=4, d_model=8, num_heads=2, num_encoder_layers=3, num_decoder_layers=3, dropout=0.1
+).to(device)
+opt = torch.optim.SGD(model.parameters(), lr=0.01)
+loss_fn = nn.CrossEntropyLoss()
+
+def train_loop(model, opt, loss_fn, dataloader):
+    model.train()
+    total_loss = 0
+    
+    for batch in dataloader:
+        X, y = batch[:, 0], batch[:, 1]
+        X, y = torch.tensor(X).to(device), torch.tensor(y).to(device)
+
+        # Now we shift the tgt by one so with the <SOS> we predict the token at pos 1
+        y_input = y[:,:-1]
+        y_expected = y[:,1:]
+        
+        # Get mask to mask out the next words
+        sequence_length = y_input.size(1)
+        tgt_mask = model.get_tgt_mask(sequence_length).to(device)
+
+        # Standard training except we pass in y_input and tgt_mask
+        pred = model(X, y_input, tgt_mask)
+
+        # Permute pred to have batch size first again
+        pred = pred.permute(1, 2, 0)      
+        loss = loss_fn(pred, y_expected)
+
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+    
+        total_loss += loss.detach().item()
+        
+    return total_loss / len(dataloader)
+
+def validation_loop(model, loss_fn, dataloader):
+    model.eval()
+    total_loss = 0
+    
+    with torch.no_grad():
+        for batch in dataloader:
+            X, y = batch[:, 0], batch[:, 1]
+            X, y = torch.tensor(X, dtype=torch.long, device=device), torch.tensor(y, dtype=torch.long, device=device)
+
+            # Now we shift the tgt by one so with the <SOS> we predict the token at pos 1
+            y_input = y[:,:-1]
+            y_expected = y[:,1:]
+            
+            # Get mask to mask out the next words
+            sequence_length = y_input.size(1)
+            tgt_mask = model.get_tgt_mask(sequence_length).to(device)
+
+            # Standard training except we pass in y_input and src_mask
+            pred = model(X, y_input, tgt_mask)
+
+            # Permute pred to have batch size first again
+            pred = pred.permute(1, 2, 0)      
+            loss = loss_fn(pred, y_expected)
+            total_loss += loss.detach().item()
+        
+    return total_loss / len(dataloader)
+
+def fit(model, opt, loss_fn, train_dataloader, val_dataloader, epochs):
+    # Used for plotting later on
+    train_loss_list, validation_loss_list = [], []
+    
+    print("Training and validating model")
+    for epoch in range(epochs):
+        epoch_start_time = time.time()
+        print("-"*25, f"Epoch {epoch + 1}","-"*25)
+        
+        train_loss = train_loop(model, opt, loss_fn, train_dataloader)
+        train_loss_list += [train_loss]
+        
+        validation_loss = validation_loop(model, loss_fn, val_dataloader)
+        validation_loss_list += [validation_loss]
+
+        elapsed = time.time() - epoch_start_time
+        
+        print(f"Training loss: {train_loss:.4f}")
+        print(f"Validation loss: {validation_loss:.4f}")
+        print(f"Time per epoch: {elapsed:.4f}")
+        print()
+        
+    return train_loss_list, validation_loss_list
+    
+train_loss_list, validation_loss_list = fit(model, opt, loss_fn, train_dataloader, val_dataloader, 10)
+
+
+# ========== Test transformer model ==========
+def predict(model, input_sequence, max_length=15, SOS_token=2, EOS_token=3):
+    model.eval()
+    
+    y_input = torch.tensor([[SOS_token]], dtype=torch.long, device=device)
+
+    num_tokens = len(input_sequence[0])
+
+    for _ in range(max_length):
+        # Get source mask
+        tgt_mask = model.get_tgt_mask(y_input.size(1)).to(device)
+        
+        pred = model(input_sequence, y_input, tgt_mask)
+        
+        next_item = pred.topk(1)[1].view(-1)[-1].item() # num with highest probability
+        next_item = torch.tensor([[next_item]], device=device)
+
+        # Concatenate previous input with predicted best word
+        y_input = torch.cat((y_input, next_item), dim=1)
+
+        # Stop if model predicts end of sentence
+        if next_item.view(-1).item() == EOS_token:
+            break
+
+    return y_input.view(-1).tolist()
+  
+  
+# Here we test some examples to observe how the model predicts
+examples = [
+    torch.tensor([[2, 0, 0, 0, 0, 0, 0, 0, 0, 3]], dtype=torch.long, device=device),
+    torch.tensor([[2, 1, 1, 1, 1, 1, 1, 1, 1, 3]], dtype=torch.long, device=device),
+    torch.tensor([[2, 1, 0, 1, 0, 1, 0, 1, 0, 3]], dtype=torch.long, device=device),
+    torch.tensor([[2, 0, 1, 0, 1, 0, 1, 0, 1, 3]], dtype=torch.long, device=device),
+    torch.tensor([[2, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 3]], dtype=torch.long, device=device),
+    torch.tensor([[2, 0, 1, 3]], dtype=torch.long, device=device)
+]
+
+for idx, example in enumerate(examples):
+    result = predict(model, example)
+    print(f"Example {idx}")
+    print(f"Input: {example.view(-1).tolist()[1:-1]}")
+    print(f"Continuation: {result[1:-1]}")
+    print()
